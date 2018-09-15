a67941c1c640bfb32003d286267b8d6930ad71e2
张宇航 <zhangyuhang@gridsum.com>
2018-09-15 18:03:18 +0800

    1、第一个实例解决

---
+++ bootplussystem/bootplusbasicapi/src/main/java/com/lifeinfinity/company/BootPlusBasicApiApplication.java
@@ -0,0 +1,18 @@
+package com.lifeinfinity.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhangyuhang@xx.com
+ * @Description:
+ * @Date: Created in 11:34 2018/9/15
+ * @Modified By:
+ */
+@SpringBootApplication
+public class BootPlusBasicApiApplication {
+    public static void main(String[] args) {
+        //Spring 应用启动起来
+        SpringApplication.run(BootPlusBasicApiApplication.class,args);
+    }
+}
